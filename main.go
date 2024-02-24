e67534d23802a310c14f74eefac81562520235a1
fuxing97 <49548695+fuxing97@users.noreply.github.com>
2024-02-25 00:07:28 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	mysql "movies_online/database"
+	"movies_online/routers"
+)
+
+func main() {
+	// 初始化数据库
+	fmt.Println("初始化数据库连接……")
+	if mysql.SqlDb != nil {
+		// 延迟退出关闭mysql
+		defer mysql.SqlDb.Close()
+	}
+	router := routers.InitRouter()
+	router.Run(":80")
+}
